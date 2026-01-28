738cd8b3a388562bdea34ba6a9287b61c1ee33f0
siddharthrpardeshi <siddharthrp1999@gmail.com>
2026-01-28 18:30:21 +0530

    Completed all Backend API's and updated code

--- DependencySystem.API/Model/ModuleTechnology.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ModuleTechnology
-{
-    public int ModuleID { get; set; }
-    public Module? Module { get; set; }
-
-    public int TechID { get; set; }
-    public Technology? Technology { get; set; }
-}
